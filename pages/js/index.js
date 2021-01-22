533bc2832de9a504f9c6906feb51635c48bda6a1
xXRagLadyXx <59878428+xXRagLadyXx@users.noreply.github.com>
2021-01-22 11:21:45 -0500

    Update index.js

--- pages/js/index.js
+++ pages/js/index.js
@@ -1,1 +1,9 @@
+let minion-amount = 1;
+let minion-speed = 1;
+const minion-speed-const = "seconds";
 
+console.log('Minion calculator launched!');
+console.log('Cuurent Values:');
+console.log(minion-amount);
+console.log(minion-speed);
+console.log(minion-speed-const);
